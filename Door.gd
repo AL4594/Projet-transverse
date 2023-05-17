292c743fef2f6718e575bd78e623c1050da12ee1
Noah-Hemon <noah.hemon@efrei.net>
2023-05-18 00:02:33 +0200

    added music + solo ice level

--- Door.gd
+++ Door.gd
@@ -14,5 +14,8 @@ func _process(delta):
 			#get_overlapping_bodies()[0].play_walk_in_animation() # 0 will be our player
 			
 func next_level():
-	get_tree().change_scene("res://icedmap.tscn")
+	if get_tree().get_current_scene().get_name() == "res://Level_debug.tscn":
+		get_tree().change_scene("res://icedmap.tscn")
+	else:
+		get_tree().change_scene("res://icedmap_solo.tscn")
 	
